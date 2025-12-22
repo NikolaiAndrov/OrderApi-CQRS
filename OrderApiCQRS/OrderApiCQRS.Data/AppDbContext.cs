345d69e6faf4a1241c805c202c0b235e4873bf2d
nikolaiandrov <nikolaiandrov@abv.bg>
2025-12-22 11:55:21 +0200

    sql server will be used instead of sqlite, dbcontext added

---
+++ OrderApiCQRS/OrderApiCQRS.Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderApiCQRS.Data.Models;
+
+namespace OrderApiCQRS.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions dbContextOptions)
+            : base(dbContextOptions)
+        {
+            
+        }
+
+        public DbSet<Order> Orders { get; set; }
+    }
+}
