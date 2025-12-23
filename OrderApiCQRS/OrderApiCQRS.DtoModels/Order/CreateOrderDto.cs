219b643b931e1759b87d7a247c8dbc9d42f227c8
nikolaiandrov <nikolaiandrov@abv.bg>
2025-12-23 19:08:31 +0200

    create order dto

---
+++ OrderApiCQRS/OrderApiCQRS.DtoModels/Order/CreateOrderDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrderApiCQRS.DtoModels.Order
+{
+    public class CreateOrderDto
+    {
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(50, MinimumLength = 2)]
+        public string CustomerFirstName { get; set; } = null!;
+
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(50, MinimumLength = 2)]
+        public string CustomerLastName { get; set; } = null!;
+
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(10, MinimumLength = 2)]
+        public string Status { get; set; } = null!;
+
+        public decimal TotalAmount { get; set; }
+    }
+}
