7cc4c6cc6efee9dc71c28c1cbe144b28580f1427
nikolaiandrov <nikolaiandrov@abv.bg>
2025-12-23 22:21:55 +0200

    ICommandHandler interface added

---
+++ OrderApiCQRS/OrderApiCQRS.Application/Features/Interfaces/ICommandHandler.cs
@@ -0,0 +1,7 @@
+﻿namespace OrderApiCQRS.Application.Features.Interfaces
+{
+    public interface ICommandHandler<TCommand, TResult> where TCommand : notnull
+    {
+        Task<TResult> HandleAsync(TCommand command);
+    }
+}
